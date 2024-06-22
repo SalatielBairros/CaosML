b9d28d91de769b3300fcdac84bfa18fa66d8e673
Salatiel Bairros <salatiel.costabairros@gmail.com>
2024-06-22 10:26:49 -0300

    DecisionTree fit implementation

--- src/tree/decision_tree_classifier.py
+++ src/tree/decision_tree_classifier.py
@@ -1,9 +1,12 @@
 import numpy as np
-from core.attribute_validation import is_numerical_attribute
+from scipy import stats
+from base_functions import is_numerical_attribute
 
 class DecisionTreeClassifier:
-    def __init__(self, entropy_type: str = 'normalized'):
+    def __init__(self, entropy_type: str = 'normalized', min_size: int = 2, max_depth: int = 5):
         self.entropy_type = entropy_type
+        self.min_size = min_size
+        self.max_depth = max_depth
 
     def __get_entropy__(self, y: np.array):
         y_size = len(y)
@@ -42,26 +45,19 @@ class DecisionTreeClassifier:
     def __split_classes__(self, X: np.array, y: np.array, attribute_index: int, split_value):
         column_to_split = X[:, attribute_index]
 
-        node = DecisionTreeClassifierNode(split_value, attribute_index)
+        node = DecisionTreeClassifierSplit(split_value, attribute_index)
         counter = 0
-        if is_numerical_attribute(split_value):
-            for column_value in column_to_split:
-                if column_value <= split_value:
-                    node.left_x.append(X[counter])
-                    node.left_y.append(y[counter])
-                else:
-                    node.right_x.append(X[counter])
-                    node.right_y.append(y[counter])
-                counter += 1
-        else:
-            for column_value in column_to_split:
-                if column_value == split_value:
-                    node.left_x.append(X[counter])
-                    node.left_y.append(y[counter])
-                else:
-                    node.right_x.append(X[counter])
-                    node.right_y.append(y[counter])
-                counter += 1
+
+        split_function = (lambda x: x <= split_value) if is_numerical_attribute(split_value) else (lambda x: x == split_value)
+        
+        for column_value in column_to_split:
+            if split_function(column_value):
+                node.left_x.append(X[counter])
+                node.left_y.append(y[counter])
+            else:
+                node.right_x.append(X[counter])
+                node.right_y.append(y[counter])
+            counter += 1        
 
         return node
 
@@ -69,7 +65,7 @@ class DecisionTreeClassifier:
         column_to_split = X[:, attribute_index]
         unique_values = np.unique(column_to_split)
 
-        best_split_node = DecisionTreeClassifierNode.empty()
+        best_split_node = DecisionTreeClassifierSplit.empty()
         for value in unique_values:
             node = self.__split_classes__(X, y, attribute_index, value)
             information_gain = self.__information_gain__(y, node.left_y, node.right_y)
@@ -82,16 +78,58 @@ class DecisionTreeClassifier:
 
     def __find_best_feature__(self, X: np.array, y: np.array):
         n_features = len(X[0])
-        best_feature_node = DecisionTreeClassifierNode.empty()
+        best_feature_node = DecisionTreeClassifierSplit.empty()
         for feature_index in range(n_features):
             best_value_split = self.__find_best_split__(X, y, feature_index)
             if best_value_split.information_gain > best_feature_node.information_gain:
                 best_feature_node = best_value_split
-            
-            
+        return best_feature_node
+    
+    def __validate__(self, X: np.array, y: np.array):
+        X_size = len(X)
+        
+        if X_size <= self.min_size:
+            raise Exception("Input size must be bigger than min_size")
+
+        if X_size != len(y):
+            raise Exception("Both X and y must have the same size")
+        
+    def __fit_node__(self, node, depth: int = 1):
+        best_split = self.__find_best_feature__(node.X, node.y)
+        if best_split.information_gain > 0:
+            node.add_children(best_split, self.min_size)
+            if (depth + 1) <= self.max_depth:
+                if node.left:
+                    self.__fit_node__(node.left, (depth + 1))
+                if node.right:
+                    self.__fit_node__(node.right, (depth + 1))
+
+    def fit(self, X: np.array, y: np.array):
+        self.__validate__(X, y)
+        root = DecisionTreeClassifierNode(X, y)
+        self.__fit_node__(root)
+        self.root = root
+        return self
+    
+    def __predict_single(self, x: np.array, node):
+        if node.left is None and node.right is None:
+            return node.vote()
+
+        if node.split_function(x[node.splited_feature_index]):
+            if node.left is None:
+                return node.vote()
+            return self.__predict_single(x, node.left)
+        else:
+            if node.right is None:
+                return node.vote()
+            return self.__predict_single(x, node.right)
+
+    def predict(self, X: np.array):
+        predictions = [self.__predict_single(x, self.root) for x in X]
+        return np.array(predictions)
 
 
-class DecisionTreeClassifierNode:
+class DecisionTreeClassifierSplit:
     def __init__(self, value, feature_index: int) -> None:
         self.left_x = []
         self.left_y = []
@@ -107,4 +145,31 @@ class DecisionTreeClassifierNode:
 
     @staticmethod
     def empty():
-        return DecisionTreeClassifierNode(0, -1)
+        return DecisionTreeClassifierSplit(0, -1)
+    
+class DecisionTreeClassifierNode:
+    def __init__(self, X: np.array, y: np.array, parent_node = None):
+        self.X = X
+        self.y = y
+        self.splited_value = None
+        self.splited_feature_index = None
+        self.left: DecisionTreeClassifierNode = None
+        self.right: DecisionTreeClassifierNode = None
+        self.parent: DecisionTreeClassifierNode = parent_node
+        self.split_function = (lambda x: x == self.splited_value) 
+
+    def add_children(self, split: DecisionTreeClassifierSplit, min_size: int):
+        self.splited_value = split.value
+        if is_numerical_attribute(split.value):
+            self.split_function = (lambda x: x <= self.splited_value)
+
+        self.splited_feature_index = split.feature_index
+
+        if len(split.left_x) >= min_size:
+            self.left = DecisionTreeClassifierNode(split.left_x, split.left_y, self)
+
+        if len(split.right_x) >= min_size:
+            self.right = DecisionTreeClassifierNode(split.right_x, split.right_y, self)
+
+    def vote(self):
+        return stats.mode(self.y)[0]
